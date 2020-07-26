505ebd83aa799b1ce3d805b3a6e0e01d9bc4a152
sharanagouda.k <sharanagouda.k@robosoftin.com>
2020-07-26 13:53:38 +0530

    error fixed

--- src/DrawerNavigator/DrawerNavigator.Style.ts
+++ src/DrawerNavigator/DrawerNavigator.Style.ts
@@ -1,14 +1,14 @@
-import { StyleSheet } from 'react-native';
-import { fontFamily, fontSize } from '../const';
+import {StyleSheet} from 'react-native';
+import {fontFamily, fontSize} from '../const';
 import ApplicationStyle from '../Themes/Application.Style';
 import colors from '../Themes/Colors';
 
 export default StyleSheet.create({
-    ...ApplicationStyle,
-    textItemMenu: {
-        fontSize: fontSize.medium,
-        fontFamily: fontFamily.regular,
-        color: colors.primary,
-        marginLeft: 12,
-    },
+  ...ApplicationStyle,
+  textItemMenu: {
+    fontSize: fontSize.medium,
+    fontFamily: fontFamily.regular,
+    color: colors.primary,
+    marginLeft: 12,
+  },
 });
--- src/Profile/Profile.Action.ts
+++ src/Profile/Profile.Action.ts
@@ -4,11 +4,11 @@ export const GET_PROFILE_SUCCESS = 'GET_PROFILE_SUCCESS';
 export const GET_PROFILE_FAIL = 'GET_PROFILE_FAIL';
 
 export const getProfileRequest = (username: string) => {
-    return { type: GET_PROFILE_REQUEST, payload: { username } };
+  return {type: GET_PROFILE_REQUEST, payload: {username}};
 };
 export const getProfileSuccess = (data: object) => {
-    return { type: GET_PROFILE_SUCCESS, payload: { data } };
+  return {type: GET_PROFILE_SUCCESS, payload: {data}};
 };
 export const getProfileFail = (err: object) => {
-    return { type: GET_PROFILE_FAIL, payload: { err } };
+  return {type: GET_PROFILE_FAIL, payload: {err}};
 };
